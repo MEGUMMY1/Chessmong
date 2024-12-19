b7e0351f1bcc408255c9d84e3d186fd770fc09b1
MEGUMMY1 <cys10902@nate.com>
2024-12-19 21:56:10 +0900

    Feat: Footer 추가

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,5 +2,21 @@ import styles from "./Layout.module.scss";
 import { LayoutProps } from "./Layout.types";
 
 export default function Layout({ children }: LayoutProps) {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.inner}>
+        {children}
+        <footer className={styles.footer}>
+          <p className={styles.reservedTxt}>© CHESSMONG, All rights reserved.</p>
+          <a
+            className={styles.contactBtn}
+            href="mailto:leemhoon000@gmail.com?subject=[체스 문의]&body=안녕하세요,%0A체스 관련해서 문의드립니다."
+            target="_blank"
+          >
+            Contact
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
