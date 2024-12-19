b230dabf1dcaca54a5235c363a6a4e187e8615ef
MEGUMMY1 <cys10902@nate.com>
2024-12-19 21:59:43 +0900

    Fix: build error

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -12,6 +12,7 @@ export default function Layout({ children }: LayoutProps) {
             className={styles.contactBtn}
             href="mailto:leemhoon000@gmail.com?subject=[체스 문의]&body=안녕하세요,%0A체스 관련해서 문의드립니다."
             target="_blank"
+            rel="noopener noreferrer"
           >
             Contact
           </a>
