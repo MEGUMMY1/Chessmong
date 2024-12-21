a1229edc2a491bfbb0c1da83e450d72ba0cbed64
MEGUMMY1 <cys10902@nate.com>
2024-12-21 17:42:49 +0900

    Feat: 유튜버 필터 추가

--- src/apis/get/getLecturesList.ts
+++ src/apis/get/getLecturesList.ts
@@ -1,20 +1,20 @@
 import { useQuery } from "react-query";
 import BASE_URL from "../../constants/BASEURL";
 
-const fetchLectures = async (fen: string) => {
-  const response = await BASE_URL.get(`/lectures`, {
-    params: { fen },
-  });
+const fetchLectures = async (fen: string, channelNames?: string) => {
+    const response = await BASE_URL.get(`/lectures`, {
+        params: { fen, channelNames },
+    });
 
-  if (response.status !== 200) {
-    throw new Error("강의 목록을 불러오는 데 실패했습니다.");
-  }
+    if (response.status !== 200) {
+        throw new Error("강의 목록을 불러오는 데 실패했습니다.");
+    }
 
-  return response.data;
+    return response.data;
 };
 
-export const useLectures = (fen: string) => {
-  return useQuery(["lectures", fen], () => fetchLectures(fen), {
-    enabled: !!fen,
-  });
+export const useLectures = (fen: string, channelNames?: string) => {
+    return useQuery(["lectures", fen, channelNames], () => fetchLectures(fen, channelNames), {
+        enabled: !!fen,
+    });
 };
