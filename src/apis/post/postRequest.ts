3608156ebc037d5fbd83176027b3ff2258cc9f80
MEGUMMY1 <cys10902@nate.com>
2024-12-23 01:26:48 +0900

    Feat: 업로드 요청 모달

--- src/apis/post/postRequest.ts
+++ src/apis/post/postRequest.ts
@@ -2,7 +2,7 @@ import { useMutation } from "react-query";
 import BASE_URL from "../../constants/BASEURL";
 
 const uploadRequest = async (link: string): Promise<void> => {
-  const response = await BASE_URL.post("/lectures/upload-request", link);
+  const response = await BASE_URL.post("/lectures/upload-request", { link });
 
   if (response.status !== 201) {
     throw new Error(response.data?.message || "유효성 검사에 실패했습니다.");
--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,9 +1,51 @@
+import { useState } from "react";
 import styles from "./Header.module.scss";
 import Logo from "../../../assets/images/logo.png";
+import Icon from "../../../assets/icons/hamburger.png";
+import useClickOutside from "../../../hooks/useClickOutside";
+import { useSetRecoilState } from "recoil";
+import { modalState } from "../../../states/modalState";
+import { useUploadRequest } from "../../../apis/post/postRequest";
 
 export default function Header() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const setModal = useSetRecoilState(modalState);
+  const { mutate } = useUploadRequest();
+
   const handleReset = () => {
     window.location.reload();
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
+
+  const dropdownRef = useClickOutside<HTMLDivElement>(() => {
+    setIsDropdownOpen(false);
+  });
+
+  const handleUploadRequest = () => {
+    setModal({
+      title: "업로드 요청",
+      type: "input",
+      placeholder: "유튜브 링크를 입력하세요",
+      onConfirm: (link?: string) => {
+        mutate(link!, {
+          onSuccess: () => {
+            setModal(null);
+          },
+          onError: () => {
+            setModal({
+              title: "업로드 요청",
+              type: "input",
+              placeholder: "유튜브 링크를 입력하세요",
+              onConfirm: () => {},
+              message: "유효한 유튜브 URL을 제출하세요.",
+            });
+          },
+        });
+      },
+    });
   };
 
   return (
@@ -17,6 +59,32 @@ export default function Header() {
             style={{ cursor: "pointer" }}
             onClick={handleReset}
           />
+          <div className={styles.hamburger} onClick={toggleDropdown}>
+            <img src={Icon} width={35} alt="햄버거 버튼" />
+          </div>
+          {isDropdownOpen && (
+            <div className={styles.dropdown} ref={dropdownRef}>
+              <div
+                className={styles.dropdownItem}
+                role="button"
+                tabIndex={0}
+                onClick={handleUploadRequest}
+              >
+                업로드 요청
+              </div>
+              <a
+                className={styles.dropdownItem}
+                href="mailto:leemhoon000@gmail.com?subject=[체스 문의]&body=안녕하세요,%0A체스 관련해서 문의드립니다."
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                기능 요청
+              </a>
+              <div className={styles.dropdownItem} role="button" tabIndex={0}>
+                후원 문의
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </header>
