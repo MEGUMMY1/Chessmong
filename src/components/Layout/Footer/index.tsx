d5d821d8e54d8f4db1dad6d4b3b46da130bed6c6
MEGUMMY1 <cys10902@nate.com>
2024-12-29 20:08:05 +0900

    Feat: 방문자 카운트 뱃지 추가

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -5,7 +5,10 @@ export default function Footer() {
     <footer className={styles.container}>
       <div className={styles.footerWrapper}>
         <a href="https://hits.seeyoufarm.com">
-          <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fchessmong.com&count_bg=%232A3A47&title_bg=%232A3A47&icon=&icon_color=%23E7E7E7&title=views&edge_flat=true" />
+          <img
+            src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fchessmong.com&count_bg=%232A3A47&title_bg=%232A3A47&icon=&icon_color=%23E7E7E7&title=views&edge_flat=true"
+            alt="count"
+          />
         </a>
         <p className={styles.reservedTxt}>© CHESSMONG, All rights reserved.</p>
       </div>
